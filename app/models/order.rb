b623381deacbd290cfe9407eadd7ef45fb35e239
Amy Lee <amy.yj.lee@gmail.com>
2017-10-17 14:18:14 -0700

    Added relationships and validations to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+
+  STATUS = ["completed", "incomplete"]
+
+  belongs_to :user
+  belongs_to :cart
+  has_many :products
+
+  validates :status, presence: true, inclusion: { in: STATUS, allow_nil: false}
 end
