ab4c475e6e6dad25af118cd1462c425b9a2df258
Amy Lee <amy.yj.lee@gmail.com>
2017-10-17 15:30:35 -0700

    Added relationships and validations to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   has_many :reviews
+  belongs_to :user
+
+  validates :name, presence: true, uniqueness: true, allow_nil: false
+  validates :category, presence: true, allow_nil: false
+  validates :price, presence: true, numericality: {only_integer: true, greater_than: 0.0}
+  validates :quantity, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
 end
