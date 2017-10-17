c1fbfc409237bd95c34e89391b8d178af8a98c85
Amy Lee <amy.yj.lee@gmail.com>
2017-10-17 14:17:26 -0700

    Added relationships and validations to cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,6 @@
 class Cart < ApplicationRecord
+  has_many :products
+  belongs_to :user
+
+  validates :product_quantity, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, allow_nil: true}
 end
