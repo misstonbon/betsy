eb65e43ffce3454827777793330ca36b0f1e193a
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-17 17:29:51 -0700

    made login/logout buttons toggle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :find_user
+  
   private
   def find_user
     if session[:user_id]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -49,6 +49,10 @@ class SessionsController < ApplicationController
   end
 
   def logout
+    session[:user_id] = nil
+    flash[:status] = :success
+    flash[:result_text] = "Successfully logged out"
+    redirect_to root_path
   end
 
 end
