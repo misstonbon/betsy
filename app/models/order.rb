ffa995df49a0f5c5b17b66833666eb1d0421ffcb
Tanja Stroble <plusdeltah@gmail.com>
2017-10-18 14:59:52 -0700

    adding some methods in orders controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,34 @@ class Order < ApplicationRecord
   has_many :products
 
   validates :status, presence: true, inclusion: { in: STATUS, allow_nil: false}
+
+
+  def total_cost
+
+    total = 0.0
+
+    self.cart_items.each do |cart_item|
+      product = Product.find_by_id(cart_item.product_id)
+      total += (product.price * product.quantity)
+    end
+    return total.round(2)
+  end
+
+  def self.change_status_to_paid(order_id)
+    order = Order.find_by_id(order_id)
+    # when i put order.id, it complains , does rails know to look by id this way?
+    order.status = "paid"
+    order.save
+  end
+
+  def quantity_adjust
+    #after order has been placed, cart items are subrtacted from original quantity of items
+    self.cart_items.each do |cart_item|
+      product = Product.find_by_id(cart_item.product_id)
+      if cart_item.quantity <= product.quantity
+        product.quantity -= cart_item.quantity
+        product.save
+      end
+    end
+  end
 end
