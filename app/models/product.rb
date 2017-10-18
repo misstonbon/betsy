d9a7abecc6d7bc93a25d23df841743ee5d94b9c3
Amy Lee <amy.yj.lee@gmail.com>
2017-10-17 19:06:57 -0700

    added user has_many products relationship to user model, possible useful product model methods basic setup

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,22 @@ class Product < ApplicationRecord
             :format => { :with => /^\d{1,4}(\.\d{0,2})?$/, multiline: true }
   validates :quantity, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
+  def self.by_category(category)
+    return self.where(category: category)
+  end
+
+  # def order_by_ratings(products)
+  #   products.order(avg_rating: avg_rating(product)).limit(10)
+  # end
+
+  def avg_rating(product)
+    if (product.reviews).count > 0
+      sum_ratings = (product.reviews).reduce(:+)
+      avg = sum_ratings/(product.reviews.count)
+      return avg
+    else
+      return nil
+    end
+  end
+
 end
