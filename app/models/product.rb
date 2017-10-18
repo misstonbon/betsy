a5b81097d1b60e75cb93a6ed5c1655257d211930
Jan Edrozo <jedrozo@gmail.com>
2017-10-18 13:54:56 -0700

    Added a seeds folder in db for product and user csv to implement rails db:seed (to seed our db with products and users)

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,7 @@ class Product < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true, allow_nil: false
   validates :category, presence: true, allow_nil: false
-  validates :price, :presence => true,
-            :numericality => true,
+  validates :price, presence: true, numericality: true,
             :format => { :with => /^\d{1,4}(\.\d{0,2})?$/, multiline: true }
   validates :quantity, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,97 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+#---------------------------------------------------
+USER_FILE = Rails.root.join('db', 'seed_data', 'users.csv')
+puts "Loading raw work data from #{USER_FILE}"
+
+user_failures = []
+CSV.foreach(USER_FILE, :headers => true) do |row|
+  user = User.new
+  user.id = row['id']
+  user.name = row['name']
+  user.email = row['email']
+  user.mailing_address = row['mailing_address']
+  user.cc_number = row['cc_number']
+  user.cc_expiration_date = row['cc_expiration_date']
+  user.cc_cvv = row['cc_cvv']
+  user.zipcode = row['zipcode']
+  user.uid = row['uid']
+  user.provider = row['provider']
+  puts "Created work: #{user.inspect}"
+  successful = user.save
+  if !successful
+    user_failures << user
+    puts user.errors
+    puts "----------"
+    user.errors.each do |column, message|
+      puts "#{column}: #{message}"
+    end
+    puts "----------"
+  end
+
+end
+
+puts "Added #{User.count} user records"
+puts "#{user_failures.length} user failed to save"
+
+
+# Since we set the primary key (the ID) manually on each of the
+# tables, we've got to tell postgres to reload the latest ID
+# values. Otherwise when we create a new record it will try
+# to start at ID 1, which will be a conflict.
+puts "Manually resetting PK sequence on each table"
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+puts "done"
+
+
+#---------------------------------------------------
+PRODUCT_FILE = Rails.root.join('db', 'seed_data', 'product_seeds.csv')
+puts "Loading raw work data from #{PRODUCT_FILE}"
+
+i = 1000
+product_failures = []
+CSV.foreach(PRODUCT_FILE, :headers => true) do |row|
+  product = Product.new
+  product.id = i
+  product.user_id = row['user_id']
+  product.category = row['category']
+  product.name = row['name']
+  product.price = row['price']
+  product.quantity = row['quantity']
+  product.description = row['description']
+  puts "Created work: #{product.inspect}"
+  successful = product.save
+  if !successful
+    product_failures << product
+    puts product.errors
+    puts "----------"
+    product.errors.each do |column, message|
+      puts "#{column}: #{message}"
+    end
+    puts "----------"
+  end
+
+  i += 1
+end
+
+puts "Added #{Product.count} product records"
+puts "#{product_failures.length} products failed to save"
+
+
+# Since we set the primary key (the ID) manually on each of the
+# tables, we've got to tell postgres to reload the latest ID
+# values. Otherwise when we create a new record it will try
+# to start at ID 1, which will be a conflict.
+puts "Manually resetting PK sequence on each table"
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+puts "done"
