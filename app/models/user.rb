1ff175065413ae820253d72e210b30a47c10c800
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-17 19:14:56 -0700

    added model validation tests for review and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :reviews
+  validates :name, presence: true
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,4 +2,23 @@ require "test_helper"
 
 describe Review do
 
+  describe "relations" do
+    let(:one) { reviews(:one) }
+
+    it "has a user" do
+      review = reviews(:one)
+      review.must_respond_to :user
+      review.must_be_kind_of Review
+      review.user.must_be_kind_of User
+    end
+  end
+
+  describe "validations" do
+    it "requires a rating" do
+      review = Review.new
+      review.valid?.must_equal false
+      review.errors.messages.must_include :rating
+    end
+  end
+
 end
