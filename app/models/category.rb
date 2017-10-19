96191c2d0127d152fc81a6149de6ec5bec7c05a9
Amy Lee <amy.yj.lee@gmail.com>
2017-10-19 16:38:18 -0700

    added category fixtures, wrote basic validation and relation tests for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,12 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :products
 
   validates :name, presence: true, uniqueness: true
+
+  # def self.to_category_hash
+  #   data = {}
+  #   self.each do |cat|
+  #     data[cat.name] = cat.products
+  #   end
+  #   return data
+  # end
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,41 @@
 require "test_helper"
 
 describe Category do
-  let(:category) { Category.new }
+  let(:food_category) { categories(:food) }
 
-  it "must be valid" do
-    value(category).must_be :valid?
+  describe "relations" do
+    it "can have many products" do
+      food_category.products << products(:chocolate)
+      food_category.must_respond_to :products
+      food_category.products.first.must_be_kind_of Product
+
+      food_category.products << products(:tears)
+      food_category.products.count.must_equal 2
+      food_category.products.each do |product|
+        product.must_be_kind_of Product
+      end
+
+      food_category.products.first.name.must_equal products(:chocolate).name
+
+    end
+
+    it "can have zero products" do
+
+    end
+  end
+
+  describe "validations" do
+    let(:new_category) { Category.new }
+
+    it "must have a name to be valid" do
+      new_category.valid?.must_equal false
+    end
+
+    it "is valid if it has a name" do
+      new_category.name = "transportation"
+      new_category.valid?.must_equal true
+    end
+
+
   end
 end
