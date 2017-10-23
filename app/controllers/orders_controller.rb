3ef4c895651b4f368dd94d65490e2d14e6976221
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-23 16:46:44 -0700

    added first place order controller test

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,20 +5,14 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find(params[:id])
+    unless @order = Order.find_by(id: params[:id])
+      render_404
+    end
   end
 
   def new
     unless @order = Order.new
       render_404
-    end
-  end
-
-  def create
-    @order = Order.new
-    @order.user_id = session[:user_id]
-    @order.status = "incomplete"
-    if @order.save
     end
   end
 
@@ -29,34 +23,13 @@ class OrdersController < ApplicationController
   end
 
   def update
-    # @order = Order.find_by_id(session[:order_id])
-    # @order.update_attributes(order_params)
-    # @order.user_id = session[:user_id]
-    # @order.status = "paid"
-    # @order.order_items.each do |item|
-    #   quantity = item.quantity
-    #   product = Product.find_by_id(item.product_id)
-    #   if product
-    #     if product.quanity < quantity
-    #       flash[:status] = :failure
-    #       flash[:message] = "Error - quantity sought exceed quantity available. Please revise your order"
-    #       render :edit
-    #     else
-    #       product.quantity -= quantity
-    #       product.save
-    #     end
-    #   end
-    # end
-    # if @order.save
-    #   render :place_order
-    # else
-    #   flash.now[:error] = "Error has occured!"
-    #   render :edit
-    # end
   end
 
   def place_order
-    @order = Order.find_by_id(session[:order_id])
+    unless
+      @order = Order.find_by_id(session[:order_id])
+      render 404
+    end
     @order.update_attributes(order_params)
     @order.user_id = session[:user_id]
     @order.status = "paid"
@@ -86,10 +59,10 @@ class OrdersController < ApplicationController
 
   end
 
-  def destroy
-    Order.destroy(params[:id])
-    redirect_to orders_path
-  end
+  # def destroy
+  #   Order.destroy(params[:id])
+  #   redirect_to orders_path
+  # end
 
   private
   def order_params
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -5,6 +5,10 @@ describe OrdersController do
   let(:item2) {order_items(:orderitem2)}
   let(:order1) {orders(:pending_order)}
   let(:order2) {orders(:paid_order)}
+  let(:tanja) {users(:tanja)}
+  let(:amy) {users(:amy)}
+  let(:chocolate) { products(:chocolate) }
+
 
   describe "Order#index" do
     it "succeeds when there are Orders" do
@@ -35,13 +39,56 @@ describe OrdersController do
     end
 
     it "renders 404 not_found for a non-existent order" do
-      bogus_order_id = Order.last.id + 1
-      get edit_order_path(bogus_order_id)
+      id  = order1.id
+      order1.destroy
+      get edit_order_path(id)
       must_respond_with :not_found
     end
   end
 
-  describe "OrderItem#create" do
+  describe "Order#place_order" do
+    it "changes order status to paid with valid inputs" do
+      id = order1.id
+      order_data = {
+        order: {
+          customer_name: order1.customer_name,
+          email: order1.email,
+          mailing_address: order1.mailing_address,
+          cc_number: order1.cc_number,
+          cc_expiration_date: order1.cc_expiration_date,
+          cc_cvv: order1.cc_cvv,
+          zipcode: order1.zipcode,
+        }
+      }
+
+      get root_path
+      order_id = session[:order_id]
+      post product_order_items_path(chocolate.id), params: { order_item: {quantity: 2, product_id: chocolate.id} }
+
+      my_order_item = OrderItem.find_by(order_id: order_id)
+
+      my_order_item.wont_be_nil
+      order = Order.find_by(id: order_id)
+      order.status.must_equal "incomplete"
+      patch place_order_path, params: order_data
+      # order1.status.must_equal "paid"
+      Order.find_by(id: order_id).status.must_equal "paid"
+
+    end
+
+    it "does not change order status to paid with valid inputs" do
+
+    end
+
+    it "deletes quantity of order_item from inventory" do
+
+    end
+
+
+    it "does not delete if quantity of order_item is more than inventory" do
+
+    end
 
   end
+
 end
