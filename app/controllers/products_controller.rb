d2016f65f59d687b189dc38707f0e2c35f632343
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-24 11:50:47 -0700

    added merchant account view page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,10 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     @product.user_id = session[:user_id]
 
+    id = @product.category.to_i 
+    cat = Category.find_by_id(id)
+    @product.categories << cat
+
     if @product.save
       flash[:status] = :success
       redirect_to product_path(@product.id)
