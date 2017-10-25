cc7337e4b62fbdf706e702324817206db349e74c
Amy Lee <amy.yj.lee@gmail.com>
2017-10-25 05:20:39 -0700

    destroyed unused carts controller

--- app/controllers/carts_controller.rb
+++
@@ -1,8 +0,0 @@
-class CartsController < ApplicationController
-
-  def show
-    @cart = OrderItem.where(order_id: session[:order_id])
-    @product = OrderItem.find_by_id(params[:id])
-  end
-
-end
--- test/controllers/carts_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-describe CartsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
-end
