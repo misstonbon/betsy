8e71534fb51052c223acc6224aa964b8c4e62a18
Amy Lee <amy.yj.lee@gmail.com>
2017-10-25 11:47:13 -0700

    added default photos through paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,12 @@
 class Product < ApplicationRecord
   STOCK = ["In Stock", "Out of Stock"]
   # This method associates the attribute ":avatar" with a file attachment
-  has_attached_file :photo, styles: {
+  has_attached_file :photo,
+  :default_url => 'glitzy_default.png',
+  styles: {
     thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
+    square: '200x200>',
+    medium: '300x300>'}
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
