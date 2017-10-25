d040df5e68552921864a1b18defd4f07fc22773f
Tanja Stroble <plusdeltah@gmail.com>
2017-10-24 18:50:19 -0700

    paperclip set up

--- db/migrate/20171024233848_add_image_to_products.rb
+++ db/migrate/20171024233848_add_image_to_products.rb
@@ -2,4 +2,12 @@ class AddImageToProducts < ActiveRecord::Migration[5.1]
   def change
     add_attachment :products, :photo
   end
+
+  def self.up
+    add_attachment :products, :photo
+  end
+
+  def self.down
+    remove_attachment :products, :photo
+  end
 end
