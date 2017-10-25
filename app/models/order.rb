5742b3a2cfd20842e3c2ad4a96ac510049990cb1
Amy Lee <amy.yj.lee@gmail.com>
2017-10-24 17:34:19 -0700

    validate presence of status for all orders, passing tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,13 @@
 class Order < ApplicationRecord
 
-  # STATUS = ["pending", "paid"]
+  STATUS = ["incomplete", "paid", "complete"]
 
   # belongs_to :user
   # belongs_to :cart
   # has_many :products
   has_many :order_items
 
-  # validates :status, presence: true, inclusion: { in: STATUS, allow_nil: false}
+  validates :status, presence: true, inclusion: { in: STATUS, allow_nil: false}
 
 # TODO figre out status for orders
 # TODO checkout will change status and add user_id
