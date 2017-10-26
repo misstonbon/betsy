7859b2cc67b00d09eadfcab01d341f15242e6ee1
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-26 09:15:42 -0700

    updated config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,10 +92,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('glitzy'),
-      access_key_id: ENV.fetch('AKIAIBPC4XWBSSECFZQA'),
-      secret_access_key: ENV.fetch('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'),
-      s3_region: ENV.fetch('us-east-1'),
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
 end
